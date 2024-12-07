a1f3d621e28f3e8a5c847c936e208cbb798ae481
CHONG WEI YANG <weiyang.wyc@gmail.com>
2024-12-07 20:38:57 +0800

    merge buy-rent

--- database/migrations/2024_03_XX_add_status_to_properties.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE properties
-ADD COLUMN status ENUM('available', 'sold', 'rented', 'cancelled') NOT NULL DEFAULT 'available',
-ADD COLUMN buyer_id bigint(20) unsigned DEFAULT NULL,
-ADD COLUMN transaction_date timestamp NULL DEFAULT NULL; 
